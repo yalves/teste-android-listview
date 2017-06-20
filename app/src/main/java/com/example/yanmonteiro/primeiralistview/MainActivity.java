02afc11250a88bde2f45c8faa151e982415d43e8
Yan Monteiro <yan.monteiro@SJ208F15.local>
2017-06-20 16:58:02 -0300

    :construction: Adiciona método OnclickListener na ListView

--- app/src/main/java/com/example/yanmonteiro/primeiralistview/MainActivity.java
+++ app/src/main/java/com/example/yanmonteiro/primeiralistview/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.yanmonteiro.primeiralistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +25,16 @@ public class MainActivity extends AppCompatActivity {
                         R.layout.segundo_layout,
                         R.id.txtView,
                         listaValores);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,
+                        "Clicou em "+ position,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        lv.setAdapter(adaptador);
     }
 }
